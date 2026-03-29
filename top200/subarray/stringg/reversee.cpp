aa1ca28d1f5ca9c3e742adced7b5f403d24a1299
ankurkushwaha212106 <ankurkushwaha212106@gmail.com>
2026-03-29 17:20:08 +0530

    size

---
+++ top200/subarray/stringg/reversee.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    string s;
+    getline(cin,s);
+    int start =0;
+    int end = s.size()-1;
+    while(start < end)
+    {
+        swap(s[start],s[end]);
+        start++;
+        end--;
+    }
+    cout<<s<<endl;
+    cout<<s.size()<<endl;
+    return 0;
+}
