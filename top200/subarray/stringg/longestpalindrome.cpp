b23040c9d1240f12f4f992a07833d5c9a469de6b
ankurkushwaha212106 <ankurkushwaha212106@gmail.com>
2026-03-30 22:34:58 +0530

    longestpalindrome

--- top200/subarray/stringg/longestpalindrome.cpp
+++ top200/subarray/stringg/longestpalindrome.cpp
@@ -1,21 +1,44 @@
 #include<bits/stdc++.h>
-using namespace std;
-int main()
-{
-     string s;
-     getline(cin , s);
-     vector<string>lower(26,0),upper(26,0);
-     for(int i=0;i<s.size();i++)
-     {
-        if(s[i] >= 'a')
-        {
+ using namespace std;
+ int main()
+ {
+    string s;
+    getline(cin,s);
+    vector<int>lower(26,0),upper(26,0);
+    for(int i=0;i<s.size();i++)
+    {
+         if(s[i] >= 'a' && s[i] <= 'z')
+         {
             lower[s[i] - 'a']++;
-        }
-        else{
+
+         }
+         else
+         {
             upper[s[i] - 'A']++;
+         }
+         int count =0;
+         bool odd = 0;
+         for(int i=0;i<26;i++)
+         {
+            if(lower[i] % 2 == 0)
+            {
+            count += lower[i];
+            }
+            else
+            {
+            count += lower[i] -1;
+            odd = 1;
+         }
         }
-
-     }
-     
-
-}
+         if(upper[i] % 2 == 0)
+            {
+            count += upper[i];
+            }
+            else{
+            count += upper[i] -1;
+            odd = 1;
+            }
+    }
+cout<<count+odd;
+return 0;
+ }
---
+++ top200/subarray/stringg/sortingthesentence.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    string s;
+    getline(cin,s);
+    vector<string>ans(10);
+    string temp;
+    int count =0,index =0;
+    while(index < s.size())
+    {
+        if(s[index] == ' ')
+        {
+            
+        }
+    }
+}
