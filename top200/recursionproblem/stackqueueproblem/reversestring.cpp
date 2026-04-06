53f2504a9467be72b35260a3c3316c803b45f03a
ankurkushwaha212106 <ankurkushwaha212106@gmail.com>
2026-04-06 19:06:05 +0530

    reverse string

--- top200/recursionproblem/stackqueueproblem/reversestring.cpp
+++ top200/recursionproblem/stackqueueproblem/reversestring.cpp
@@ -7,14 +7,14 @@ int main()
     stack<char>st;
     for(int i=0;i<s.size();i++)
     {
-        st.push(s[i]);
+     st.push(s[i]);
     }
     int i=0;
     while(!st.empty())
     {
-   s[i] = st.top();
-   i++;
-   st.pop();
+        s[i] = st.top();
+        i++;
+        st.pop();
     }
     for(int i=0;i<s.size();i++)
     {
