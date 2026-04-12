e3284637bb0fc37199262190fa28404a2591b499
ankurkushwaha212106 <ankurkushwaha212106@gmail.com>
2026-04-12 21:03:07 +0530

    anticlockwise

---
+++ anticlockwise.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n = 3;
+    int a[3][3] = {
+        {1, 2, 3},
+        {4, 5, 6},
+        {7, 8, 9}
+    };
+
+    // Step 1: Transpose
+    for(int i = 0; i < n; i++) {
+        for(int j = i; j < n; j++) {
+            swap(a[i][j], a[j][i]);
+        }
+    }
+
+    // Step 2: Reverse each column
+    for(int j = 0; j < n; j++) {
+        for(int i = 0; i < n/2; i++) {
+            swap(a[i][j], a[n - i - 1][j]);
+        }
+    }
+
+    // Print result
+    for(int i = 0; i < n; i++) {
+        for(int j = 0; j < n; j++) {
+            cout << a[i][j] << " ";
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
