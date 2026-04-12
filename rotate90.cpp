68eb50f62a45fff46799f83e6f46d5604a91d417
ankurkushwaha212106 <ankurkushwaha212106@gmail.com>
2026-04-12 21:00:48 +0530

    90 degree rotate

---
+++ rotate90.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n = 3;
+    int matrix[3][3] = {
+        {1, 2, 3},
+        {4, 5, 6},
+        {7, 8, 9}
+    };
+
+    // Step 1: Transpose
+    for(int i = 0; i < n; i++) {
+        for(int j = i; j < n; j++) {
+            swap(matrix[i][j], matrix[j][i]);
+        }
+    }
+
+    // Step 2: Reverse each row
+    for(int i = 0; i < n; i++) {
+        int start = 0, end = n - 1;
+        while(start < end) {
+            swap(matrix[i][start], matrix[i][end]);
+            start++;
+            end--;
+        }
+    }
+
+    // Print result
+    for(int i = 0; i < n; i++) {
+        for(int j = 0; j < n; j++) {
+            cout << matrix[i][j] << " ";
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
