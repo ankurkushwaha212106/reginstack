579484c68b6bfbbb7e111f9b2387d69a451bac41
ankurkushwaha212106 <ankurkushwaha212106@gmail.com>
2026-04-14 21:43:56 +0530

    transpose

---
+++ transposee.cpp
@@ -0,0 +1,9 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+for(int j = 0; j < m; j++) {
+    for(int i = 0; i < n; i++) {
+        cout << arr[i][j] << " ";
+    }
+    cout << endl;
+}
