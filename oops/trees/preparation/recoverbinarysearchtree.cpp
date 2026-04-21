092299c15b846ede329ba4bf7d7fadd61ac6354f
ankurkushwaha212106 <ankurkushwaha212106@gmail.com>
2026-04-21 22:35:13 +0530

    recover

---
+++ oops/trees/preparation/recoverbinarysearchtree.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    TreeNode* first = NULL;
+    TreeNode* second = NULL;
+    TreeNode* prev = new TreeNode(INT_MIN);
+
+    void inorder(TreeNode* root) {
+        if(!root) return;
+
+        inorder(root->left);
+
+        if(prev->val > root->val) {
+            if(first == NULL) {
+                first = prev;
+            }
+            second = root;
+        }
+        prev = root;
+
+        inorder(root->right);
+    }
+
+    void recoverTree(TreeNode* root) {
+        inorder(root);
+        swap(first->val, second->val);
+    }
+};
