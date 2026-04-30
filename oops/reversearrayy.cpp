09fde09dbe25b3cc8ce1d938ef39a174f6648738
ankurkushwaha212106 <ankurkushwaha212106@gmail.com>
2026-04-30 09:23:33 +0530

    reverse

---
+++ oops/reversearrayy.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    vector<int>arr = {1,2,3,4,5};
+      stack<int>st;
+      for(int i=0;i<arr.size();i++)
+      {
+        st.push(arr[i]);
+      }
+      int i=0;
+      while(!st.empty())
+      {
+        arr[i] = st.top();
+        i++;
+        st.pop();
+      }
+      for(int i=0;i<arr.size();i++)
+      {
+        cout<<arr[i]<<"  ";
+      }
+      return 0;
+}
